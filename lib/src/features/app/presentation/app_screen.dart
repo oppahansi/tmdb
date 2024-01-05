843cc37ccd5443e9b720a46f9db9e6fb4938bda7
oppahansi <oppahansi@gmail.com>
2024-01-05 16:47:51 +0100

    feat: added app screen with bottom navigation bar

---
+++ lib/src/features/app/presentation/app_screen.dart
@@ -0,0 +1,63 @@
+// Flutter Imports
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Project Imports
+import 'package:oppa_tmdb/src/features/app/providers/screen_index_provider.dart';
+import 'package:oppa_tmdb/src/features/home/presentation/home_screen.dart';
+import 'package:oppa_tmdb/src/features/movies/presentation/movies_screen.dart';
+import 'package:oppa_tmdb/src/features/people/presentation/people_screen.dart';
+import 'package:oppa_tmdb/src/features/tv_shows/presentation/tv_shows_screen.dart';
+
+class AppScreen extends ConsumerWidget {
+  AppScreen({super.key});
+
+  final screens = [
+    const HomeScreen(),
+    const MoviesScreen(),
+    const TvShowsScreen(),
+    const PeopleScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final screenIndex = ref.watch(screenIndexProvider);
+    final primaryColor = Theme.of(context).colorScheme.primary;
+    final secondaryColor = Theme.of(context).colorScheme.secondary;
+
+    return Scaffold(
+      body: screens[screenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: screenIndex,
+        onTap: (index) {
+          ref.read(screenIndexProvider.notifier).update((state) => index);
+        },
+        showUnselectedLabels: true,
+        selectedItemColor: primaryColor,
+        unselectedItemColor: secondaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            activeIcon: Icon(Icons.home_outlined),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            activeIcon: Icon(Icons.movie_outlined),
+            label: "Movies",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv),
+            activeIcon: Icon(Icons.tv_outlined),
+            label: "TV Shows",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            activeIcon: Icon(Icons.people_outlined),
+            label: "People",
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/features/app/providers/screen_index_provider.dart
@@ -0,0 +1,6 @@
+// Flutter Imports
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final screenIndexProvider = StateProvider<int>((ref) {
+  return 0;
+});
